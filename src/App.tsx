c7577fbfc963ed5d0c300a73239a016ab7e71d47
Andre Ferreira <andrebmx789@gmail.com>
2022-08-25 01:12:33 -0300

    feat: add list page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { useEffect, useState } from 'react'
+import { UserServices } from './services/user'
+import { IUser } from './interfaces/user'
+
 function App() {
-  return <h1>Hello world!</h1>
+  const [users, setUsers] = useState<Array<IUser>>([])
+
+  const getUsers = async () => {
+    const response = await UserServices.getUsers()
+    setUsers(response)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <>
+      <ul>
+        {users.map((user: IUser) => {
+          return <li key={user.email}>{user.email}</li>
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default App
