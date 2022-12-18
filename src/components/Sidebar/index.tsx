756d99a31f3a320b86c134cde7363245afb07c52
Andre Ferreira <andrebmx789@gmail.com>
2022-12-18 05:23:22 -0300

    feat: add react-modal

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom'
+import Modal from 'react-modal'
 import { AiFillHome } from 'react-icons/ai'
 import { VscFiles } from 'react-icons/vsc'
 import { ImInfo } from 'react-icons/im'
@@ -6,45 +7,58 @@ import { Item, ItemLink, Wrapper } from './styleds'
 import { useSidebarContext } from '../../contexts/sidebarContext'
 import { useWindowSize } from '../../hooks/useWindowSize'
 import { ROUTES } from '../../constants/routes'
+import { useState } from 'react'
 
 export const Sidebar = () => {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
   const { sidebarStatus } = useSidebarContext()
   const { windowSize } = useWindowSize()
   const location = useLocation()
 
+  const handleModal = () => {
+    setIsModalOpen((oldValue) => !oldValue)
+  }
+
   return (
-    <Wrapper
-      status={sidebarStatus}
-      smarthphone={windowSize.isDeviceSmarthphone}
-    >
-      <ItemLink
-        to={ROUTES.home.path}
+    <>
+      <Modal isOpen={isModalOpen} onRequestClose={handleModal}></Modal>
+      <Wrapper
         status={sidebarStatus}
-        active={location.pathname === ROUTES.home.path}
         smarthphone={windowSize.isDeviceSmarthphone}
       >
-        <p>Início</p>
-        <span>
-          <AiFillHome size="100%" />
-        </span>
-      </ItemLink>
-      <ItemLink
-        to={ROUTES.docs.path}
-        status={sidebarStatus}
-        active={location.pathname === ROUTES.docs.path}
-        smarthphone={windowSize.isDeviceSmarthphone}
-      >
-        <p>Meus Documentos</p>
-        <span>
-          <VscFiles size="100%" />
-        </span>
-      </ItemLink>
-      <Item status={sidebarStatus} smarthphone={windowSize.isDeviceSmarthphone}>
-        <p>Sobre</p>
-        <span>
-          <ImInfo size="100%" />
-        </span>
-      </Item>
-    </Wrapper>
+        <ItemLink
+          to={ROUTES.home.path}
+          status={sidebarStatus}
+          active={location.pathname === ROUTES.home.path}
+          smarthphone={windowSize.isDeviceSmarthphone}
+        >
+          <p>Início</p>
+          <span>
+            <AiFillHome size="100%" />
+          </span>
+        </ItemLink>
+        <ItemLink
+          to={ROUTES.docs.path}
+          status={sidebarStatus}
+          active={location.pathname === ROUTES.docs.path}
+          smarthphone={windowSize.isDeviceSmarthphone}
+        >
+          <p>Meus Documentos</p>
+          <span>
+            <VscFiles size="100%" />
+          </span>
+        </ItemLink>
+        <Item
+          onClick={handleModal}
+          status={sidebarStatus}
+          smarthphone={windowSize.isDeviceSmarthphone}
+        >
+          <p>Sobre</p>
+          <span>
+            <ImInfo size="100%" />
+          </span>
+        </Item>
+      </Wrapper>
+    </>
   )
 }
