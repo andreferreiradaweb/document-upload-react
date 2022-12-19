c8beb7bdf9af0b9297abc9a877582c0f22bbb8fa
Andre Ferreira <andrebmx789@gmail.com>
2022-12-18 23:44:30 -0300

    feat: add elements to home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,9 +1,40 @@
+import Ilustration from '../../assets/ilustration-about.svg'
 import { Layout } from '../../components/Layout'
+import {
+  ImageElement,
+  ImageWrapper,
+  Wrapper,
+  Content,
+  TitleElement,
+  Button,
+  TextElement,
+  ListElement,
+  ItemElement,
+} from './styles'
 
 export const HomePage = () => {
   return (
     <Layout>
-      <h1>test</h1>
+      <Wrapper>
+        <Content>
+          <TitleElement>Conheça as vantagens do nosso serviço</TitleElement>
+          <TextElement>
+            Facilite o processo de envio de documentos com o melhor serviço de
+            upload existente no mercado, conheça as vantagens de utilizar nossos
+            serviços listadas abaixo:
+          </TextElement>
+          <ListElement>
+            <ItemElement>Segurança</ItemElement>
+            <ItemElement>Conforto</ItemElement>
+            <ItemElement>Velocidade</ItemElement>
+            <ItemElement>Qualidade</ItemElement>
+          </ListElement>
+          <Button>Assinar Premium</Button>
+        </Content>
+        <ImageWrapper>
+          <ImageElement src={Ilustration} alt="" />
+        </ImageWrapper>
+      </Wrapper>
     </Layout>
   )
 }
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -1,8 +1,84 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-  max-width: 900px;
-  height: 100vh;
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
   padding: 0 15px;
-  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 150px;
+
+  @media screen and (max-width: 900px) {
+    width: 100%;
+    flex-direction: column;
+    gap: 40px;
+  }
+`
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 45%;
+
+  @media screen and (max-width: 900px) {
+    max-width: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+`
+
+export const ImageWrapper = styled.div`
+  width: 400px;
+
+  @media screen and (max-width: 900px) {
+    width: 300px;
+  }
+`
+
+export const ImageElement = styled.img`
+  width: 100%;
+  height: 100%;
+`
+
+export const TitleElement = styled.h1`
+  font-size: 2rem;
+  color: var(--gray-text);
+  margin-top: 0;
+
+  @media screen and (max-width: 900px) {
+    text-align: center;
+  }
+`
+
+export const Button = styled.button`
+  background-color: var(--blue-light);
+  border: none;
+  padding: 7px 10px;
+  width: 200px;
+  border-radius: 8px;
+  color: var(--white);
+`
+
+export const TextElement = styled.div`
+  @media screen and (max-width: 900px) {
+    text-align: center;
+    max-width: 400px;
+  }
+`
+
+export const ListElement = styled.ul`
+  margin-left: -25px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+
+  @media screen and (max-width: 900px) {
+    margin-top: 10px;
+    margin-bottom: 10px;
+  }
+`
+
+export const ItemElement = styled.li`
+  margin-left: 0;
 `
